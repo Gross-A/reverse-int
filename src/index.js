bfad942f6c22af65439247545103f068bc3f4e67
Aleksandr_G <shura-35@yandex.ru>
2022-01-06 08:45:12 +0300

    fix: change index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
+    let absoluteN = Math.abs(n);
+    let reversInt = '';
+    console.log (typeof n);
+    let stringN = absoluteN.toString();
+    console.log (typeof stringN);
+    for (let i = (stringN.length -1); i >= 0; i--){
+      reversInt += stringN[i];
+    }
+//   console.log(reversInt);
+//   return reversInt;
+
+    return reversInt;
   
 }
